6857ef66229c8abe2e415407aa683a06e0036ac7
unknown <ardiansyah@fuse.co.id>
2025-02-14 11:22:14 +0700

    test build

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,7 @@ let mix = require('laravel-mix');
 
 // Kompilasi file Sass dan JavaScript
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .sass('public/css');
 
 // Menambahkan versi file untuk caching browser
 mix.version();
