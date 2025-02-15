98e7672106aa391f43cde068b6c63beb5ed33e03
unknown <ardiansyah@fuse.co.id>
2025-02-16 04:05:19 +0700

    edit bash

--- build-app.sh
+++ build-app.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+rm public/storage
 php artisan storage:link
 chmod -R 775 storage bootstrap/cache
 chmod -R 775 storage app
+
 # chown -R 33:33 /var/www/html/storage/app/public && echo "✅ added permissions to mounted volume"
 # php /var/www/html/artisan filament:optimize
